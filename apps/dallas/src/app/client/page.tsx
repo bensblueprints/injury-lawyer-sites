aaaf09a78f7807bf597f0f41f301a08742ecc41e
Ben <ben@advancedmarketing.co>
2026-05-13 16:09:11 +0700

    Add /admin and /client portal redirect routes to all 5 sites

---
+++ apps/dallas/src/app/client/page.tsx
@@ -0,0 +1,6 @@
+﻿import { redirect } from "next/navigation";
+
+export default function ClientPortalRedirect() {
+  redirect("https://getlegalleads.xyz/attorney/login");
+}
+
---
+++ apps/las-vegas/src/app/client/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function ClientPortalRedirect() {
+  redirect("https://getlegalleads.xyz/attorney/login");
+}
